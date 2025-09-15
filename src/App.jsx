9d257f3da4b8bb560014b7011b05aa8a64a8ae01
Inayat Fatima Qureshi <inayat.fatima.qureshi@gmail.com>
2025-09-15 13:02:30 +0800

    added basic tailwind for home body, started with the structure for sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Sidebar from './components/Layout/Sidebar'
+import Header from './components/Layout/Header'
 
 function App() {
   return (
-    <div>
-      App
+    <div 
+      className='
+        min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50 dark:from-slate-900 dark:via-slate-800 
+        dark:to-slate-900 transition-all duration-500 
+      '
+    >
+      <div className='flex h-screen overflow-hidden'>
+        <Sidebar/>
+      </div>
     </div>
   )
 }
